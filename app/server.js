aaa7d0facb86b65acf91f6b005469de33cd1926f
kodziarz <58184304+kodziarz@users.noreply.github.com>
2022-05-23 06:41:04 +0200

    Delete app directory

--- app/server.js
+++
@@ -1,8 +0,0 @@
-const logger = require('tracer').colorConsole()
-const http = require('http');
-const router = require("./app/router")
-const PORT = 3000
-
-http
-    .createServer((req, res) => router(req, res))
-    .listen(PORT, () => logger.info("Serwer wystratował na porcie: ", PORT))
